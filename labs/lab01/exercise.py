aee0f1b6439e1845e8a1a99796992103a9c2f1b3
Shahfareeze <faris07shah@gmail.com>
2025-07-15 12:28:10 +0800

    save file

--- labs/lab01/exercise.py
+++ labs/lab01/exercise.py
@@ -1,5 +1,5 @@
 # labs/lab01/exercise.py
 # Starter code for Lab 01
-print("Hello everyones and i use python use the python, Lab 01")
+print("Hello everyone and i use python use the python, Lab 01")
 
 
