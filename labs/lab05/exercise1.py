7f1954ad43223f6302685c5148c50e3bc0ab887b
Shahfareeze <faris07shah@gmail.com>
2025-08-05 12:05:19 +0800

    python excercise

---
+++ labs/lab05/exercise1.py
@@ -0,0 +1,20 @@
+# INPUT USER INFO
+
+User_Name = input ("Enter your name:")
+Age = int (input ("Enter your age:"))
+Course_Code = input("Enter your course code:")
+
+# OUTPUT
+
+print()
+print ("User name:" + User_Name)
+print ("Age: " + str(Age) )
+print ("Course Code: " + Course_Code)
+
+# DATA TYPES
+
+print(type(User_Name))
+print(type(Age))
+print(type(Course_Code))
+
+
