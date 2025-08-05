c4cb69e7ea8c83d280e04727c5f36b44e029cf31
Shahfareeze <faris07shah@gmail.com>
2025-08-05 12:50:43 +0800

    up

--- labs/lab05/exercise3.py
+++ labs/lab05/exercise3.py
@@ -1,3 +1,10 @@
 # import
 import random
+
+# Input for information
 class_name = input("Enter class name: ")
+random_number = random.randint(1, 100)
+
+# Output 
+print("Class Name: " + class_name)
+print ("Random number: " +str(random_number))
