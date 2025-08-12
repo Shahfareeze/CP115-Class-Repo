da3b3d0a0178c356761765910956ba885896405b
Shahfareeze <faris07shah@gmail.com>
2025-08-12 12:16:31 +0800

    lab 6 update

--- labs/lab06/exercise3.py
+++ labs/lab06/exercise3.py
@@ -1,7 +1,8 @@
 user_name = input("Enter your full name:")
 uppercase_user_name = user_name.upper()
 lowercase_user_name = user_name.lower()
-print(uppercase_user_name)
-print(lowercase_user_name)
-print(len(user_name))
+length_user_name = (len(user_name))
+print("Uppercase: " + uppercase_user_name)
+print("Lowercase: " + lowercase_user_name)
+print("Name Length: " + str(length_user_name))
 
