06bcb0828f9d39bb0c11a1d48991dd3cea4880c9
Shahfareeze <faris07shah@gmail.com>
2025-08-12 12:58:00 +0800

    lab test

---
+++ lab-test-2.py
@@ -0,0 +1,11 @@
+#Programmer's name:Shahfareeze Bin Ramlee
+#Problem: Get the same output as in the test and have arithmetic expression
+
+
+shape = "*\t\t\t*\n**\t\t**\n***\t***\n********\n***\t***\n**\t\t**\n*\t\t\t*"
+marks = 2*10
+
+print("Name: Ahmad Bin Abu\t\tMatric.No:MS2025123499")
+print(shape)
+print("This is my\n\tsecond\n\t\tassignment")
+print("I want 2x10 marks, which is " + str(marks), "full marks")
