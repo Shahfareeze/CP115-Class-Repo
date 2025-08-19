f7eadb5960a0892fc909277776683d3b555cc89e
Shahfareeze <faris07shah@gmail.com>
2025-08-19 13:09:44 +0800

    exercise 4

---
+++ labs/lab07/exercise4/exercise4.py
@@ -0,0 +1,9 @@
+import menu_data
+
+print(f"Restaurant Name: {menu_data.restaurant_name}")
+print(f"Restaurant Location: {menu_data.location}")
+print(f"Restaurant menu: {menu_data.menu1,menu_data.menu2,menu_data.menu3}")
+print(f"Restaurant Name: {menu_data.restaurant_name}")
+print(f"Restaurant Name: {menu_data.restaurant_name}")
+print(f"Restaurant Name: {menu_data.restaurant_name}")
+print(f"Restaurant Name: {menu_data.restaurant_name}")
---
+++ labs/lab07/exercise4/menu_data.py
@@ -0,0 +1,18 @@
+import random
+
+restaurant_name = "CITA RASA SABAH"
+location = "Sipitang,Sabah"
+#Menu name 
+menu1 = "Nasi Goreng Ayam/Daging/Seafood"
+menu2 = "Nasi Goreng Pataya"
+menu3 = "Mee Goreng Ayam/Daging/Seafood"
+
+#String Operation
+upper = restaurant_name.upper()
+lower = restaurant_name.lower()
+length = len(restaurant_name)
+
+#random
+special_number = random.randrange(1,3)
+customer_number = random.randrange(100,999)
+
