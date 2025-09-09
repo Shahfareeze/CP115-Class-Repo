221be5732575b3d61fbdfc85f4082b5fc9d857fb
Shahfareeze <faris07shah@gmail.com>
2025-09-09 12:08:49 +0800

    update

--- labs/lab09/exercise2/exercise2.py
+++ labs/lab09/exercise2/exercise2.py
@@ -1,32 +1,37 @@
-employee_name = input()
-base_salary = int(input())
-overtime_hours = int(input())
-tax_status = input()
+employee_name = input("Enter your name:" )
+
+base_salary = input("Enter your salary:" )
+
+overtime_hours = input("Enter your overtime:")
+
+tax_status = input("Enter your status (single/married/head):")
 
 # TODO your code here
+base_salary = float(base_salary)
+overtime_hours = float(overtime_hours)
 
-tax_rate = ""
+tax = ""
 
-if tax_status == "single":
-    if base_salary >= 5000 :
-        tax_rate = 0.22
-    else :
-        tax_rate = 0.18
-elif tax_status == "married" :
-    if base_salary >= 6000 :
-        tax_rate = 0.2
-    else:
-        tax_rate = 0.15
-elif tax_status == "head" :
-    if base_salary >=5500 :
-        tax_rate = 0.25
-    else:
-        tax_rate = 0.19
+if tax_status == "single" and base_salary >= 5000 :
+    tax = 0.22
+else :
+    tax = 0.18
+if tax_status == "married" and base_salary >= 6000:
+    tax = 0.2
+else:
+    tax = 0.15
+if tax_status == "head"  and base_salary >=5500 :
+    tax = 0.25
+else:
+    tax = 0.19
 
 additional_deduction = 0.11 + 0.005
-net_salary = base_salary + (base_salary * tax_rate) + (base_salary*additional_deduction) + (35 * overtime_hours)
+tax_rate = additional_deduction + tax
+tax_salary = (base_salary * tax_rate)
+overtime_salary = 35 * overtime_hours
+net_salary = (base_salary - tax_salary) + overtime_salary
         
 
 print(employee_name)
-print(f"{tax_rate}")
+print(tax_rate)
 print(f"{net_salary:.2f}")
