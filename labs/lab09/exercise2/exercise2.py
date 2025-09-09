aa1b9aad478133eac0dbee2e59fff39caf98c2fd
Shahfareeze <faris07shah@gmail.com>
2025-09-09 12:25:05 +0800

    up

--- labs/lab09/exercise2/exercise2.py
+++ labs/lab09/exercise2/exercise2.py
@@ -10,28 +10,27 @@ tax_status = input("Enter your status (single/married/head):")
 base_salary = float(base_salary)
 overtime_hours = float(overtime_hours)
 
-tax = ""
+tax_rate = ""
 
 if tax_status == "single" and base_salary >= 5000 :
-    tax = 0.22
+    tax_rate = 0.22
 else :
-    tax = 0.18
+    tax_rate = 0.18
 if tax_status == "married" and base_salary >= 6000:
-    tax = 0.2
+    tax_rate = 0.2
 else:
-    tax = 0.15
+    tax_rate = 0.15
 if tax_status == "head"  and base_salary >=5500 :
-    tax = 0.25
+    tax_rate= 0.25
 else:
-    tax = 0.19
+    tax_rate = 0.19
 
 additional_deduction = 0.11 + 0.005
-tax_rate = additional_deduction + tax
-tax_salary = (base_salary * tax_rate)
 overtime_salary = 35 * overtime_hours
-net_salary = (base_salary - tax_salary) + overtime_salary
-        
+total_salary = base_salary + overtime_salary
+net_salary = total_salary * (1 - tax_rate) * (1 - additional_deduction)
+
 
 print(employee_name)
-print(tax_rate)
+print(f"{tax_rate * 100}%")
 print(f"{net_salary:.2f}")
