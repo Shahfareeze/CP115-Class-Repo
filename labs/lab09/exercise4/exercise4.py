4d6eb32c527e6183428c0a8150efec02f490ae43
Shahfareeze <faris07shah@gmail.com>
2025-09-23 12:21:57 +0800

    lab10

--- labs/lab09/exercise4/exercise4.py
+++ labs/lab09/exercise4/exercise4.py
@@ -3,6 +3,13 @@ previous_reading = int(input())
 
 # TODO: Your code here
 
+cubic_meter = current_reading
+
+if cubic_meter <=20:
+
+ cubic_meter
+water_cost = 10
+
 print(consumption)
 print(water_cost)
 print(total_bill)
--- labs/lab10/exercise1/exercise1.py
+++ labs/lab10/exercise1/exercise1.py
@@ -4,6 +4,35 @@ is_weekend = input()
 
 # Your code here
 
+base_hourly_rate = 0
+hourly_rate = 0
+overtime_pay = 0
+total_pay = 0
+
+
+if position == "Manager":
+    base_hourly_rate = 35 
+    
+elif position == "Supervisor":
+    base_hourly_rate = 25
+    
+elif position == "Staff":
+    base_hourly_rate = 18
+    
+
+
+
+bonus_weekend = 0
+
+if is_weekend == "Yes" :
+    bonus_weekend_pay =  (overtime_hours * 5)
+elif is_weekend == "No":
+    bonus_weekend_pay = 0
+
+hourly_rate = base_hourly_rate
+overtime_pay = ((overtime_hours * base_hourly_rate) * 1.5 )+ bonus_weekend_pay
+total_pay = overtime_pay 
+
 print(hourly_rate)
 print(overtime_pay)
 print(total_pay)
