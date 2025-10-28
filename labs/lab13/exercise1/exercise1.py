9e306d1b1a4f8a5e081ad5bf9da83f60aedf2826
Shahfareeze <faris07shah@gmail.com>
2025-10-28 12:11:31 +0800

    lab13

--- labs/lab13/exercise1/exercise1.py
+++ labs/lab13/exercise1/exercise1.py
@@ -2,5 +2,21 @@ correct_password = "python123"
 
 # TODO: Your code here
 
+attempts_used = 1
+login_successful = False
+
+password = input()
+while attempts_used < 3:
+    
+    attempts_used += 1
+    password = input()
+
+    if password == correct_password:
+        login_successful = True
+        break
+
+    
+
+
 print(login_successful)
 print(attempts_used)
